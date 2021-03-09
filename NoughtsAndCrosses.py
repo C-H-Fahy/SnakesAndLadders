926ec777fbb098d876821d732e9280b6dcfbd92d
bunsen <bunsen@localhost.localdomain>
2021-03-09 00:44:58 +0000

    added grid drawing

---
+++ NoughtsAndCrosses.py
@@ -0,0 +1,32 @@
+#External
+import turtle
+import time
+#Internal
+import constants
+
+def vertline(x, startpoint):
+    """draws a verticle line of  500"""
+    turtle.penup()
+    turtle.setpos(x, 0)
+    turtle.setheading(90)
+    turtle.pendown()
+    turtle.forward(500)
+    
+def horiline(y, startpoint):
+    """draws a horizontal line of  500"""
+    turtle.penup()
+    turtle.setpos(0, y)
+    turtle.setheading(0)
+    turtle.pendown()
+    turtle.forward(500)
+
+def drawgrid(startpoint):
+    for i in range(0, constants.GRID+1):
+        vertline(i*(500//constants.GRID), startpoint)
+        horiline(i*(500//constants.GRID), startpoint)
+      
+turtle.screensize(1000, 1000)  
+        
+drawgrid(0)
+a = input()
+
---
+++ constants.py
@@ -0,0 +1,7 @@
+#size of grid
+GRID = 5
+
+#Each entry is location of Snake on grid (Startpoint/TriggerSquare, Endpoint) 
+SNAKES = [(1, 1), 
+
+#Each entry is location of Ladder on grid (Startpoint/TriggerSquare, Endpoint) 
