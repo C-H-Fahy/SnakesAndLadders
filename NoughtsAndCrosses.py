c5760b4dd1549503a4ff049fcea0bed649521e10
CHFahy <ciaran@ciaranfahy.com>
2021-03-09 02:19:48 +0000

    added numbering

--- NoughtsAndCrosses.py
+++ NoughtsAndCrosses.py
@@ -11,7 +11,7 @@ def vertline(x):
     turtle.setheading(90)
     turtle.pendown()
     turtle.forward(500)
-    
+
 def horiline(y):
     """draws a horizontal line of  500"""
     turtle.penup()
@@ -20,13 +20,35 @@ def horiline(y):
     turtle.pendown()
     turtle.forward(500)
 
-def drawgrid(startpoint):
-    for i in range(0, constants.GRID+1):
-        vertline(i*(500//constants.GRID))
-        horiline(i*(500//constants.GRID))
-      
-turtle.screensize(1000, 1000)  
-        
-drawgrid(0)
-a = input()
+def drawmap():
+    """Draws Out All The Fixed Stuff, Outputs list with the centres of Squares"""
+    gap = 500//constants.GRID
+    #Finds positions of all squares 
+    positions = []
+    for i in range(0, constants.GRID):
+        y = gap * i + gap/2
+        for i in range(0, constants.GRID):
+            x = gap * i + gap/2
+            positions.append((x, y))
 
+    #Drawing        
+    for i in range(0, constants.GRID+1):
+        vertline(i*gap)
+        horiline(i*gap)
+        
+    for i in range(0, len(positions)):
+        turtle.penup()
+        turtle.setpos(positions[i])
+        turtle.pendown()
+        turtle.write(i)
+ 
+    return(positions)
+
+def main():
+    positions = drawmap()
+    print(positions)
+
+turtle.screensize(1000, 1000)
+main()
+
+a = input()
--- constants.py
+++ constants.py
@@ -2,6 +2,4 @@
 GRID = 5
 
 #Each entry is location of Snake on grid (Startpoint/TriggerSquare, Endpoint) 
-SNAKES = [(1, 1), 
-
 #Each entry is location of Ladder on grid (Startpoint/TriggerSquare, Endpoint) 
