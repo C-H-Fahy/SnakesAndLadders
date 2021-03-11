2ac1bb9b264ef378700f81fce2e434c11a7e4fdb
CHFahy <ciaran@ciaranfahy.com>
2021-03-11 15:07:39 +0000

    Improved efficency of NaughtsAndCrosses.FindPos()

--- NoughtsAndCrosses.py
+++ NoughtsAndCrosses.py
@@ -13,17 +13,12 @@ import shapes
 turtle.screensize(1000, 1000)
 
 def FindPos(n):
-    """Finds centre of square, returns None if square does not exist"""
+    """Finds centre of square"""
     gap = config.SIZE//config.GRID
-    a = 0
-    for i in range(0, config.GRID):
-        y = (gap * i + gap/2) + config.GRIDPOS[1]
-        for i in range(0, config.GRID):
-            x = (gap * i + gap/2) + config.GRIDPOS[0]
-            if a == n:
-                return (x, y)
-            a = a + 1
-
+    x = (n % config.GRID) * gap + gap/2
+    y = (n // config.GRID) * gap + gap/2
+    return(x, y)
+    
 def DrawMap():
     """Draws Out All The Fixed Stuff"""
     turtle.speed(config.DRAW_SPEED)
@@ -63,21 +58,14 @@ def turn(player, title, pos, offset):
     esc = input("Player "+ title + " turn:\n")
     move = random.randint(1, 6)
     print(move)
-    try: 
-        #Move Player
-        pos = move + pos
-        (x, y) = FindPos(pos)
-        x = x + offset
-        player.setpos(x, y)
-    except TypeError:
-        #When FindPos returns None
-        pos = config.GRID * config.GRID - 1
-        (x, y) = FindPos(pos)
-        x = x + offset
-        player.setpos(x, y)
+    #Move Player
+    pos = move + pos
+    (x, y) = FindPos(pos)
+    x = x + offset
+    player.setpos(x, y)
         
     #Check to see if player is on Ladder and move them if they are
-    for i in range (0,len(config.LADDERS)):
+    for i in range (0, len(config.LADDERS)):
         if pos == config.LADDERS[i][0]:
             print(title + " gets ladder from " + str(config.LADDERS[i][0]) + " to " + 
             str(config.LADDERS[i][1]))
@@ -88,7 +76,7 @@ def turn(player, title, pos, offset):
             player.setpos(x, y)
             
     #Check to see if player is on Snake and move them if they are
-    for i in range (0,len(config.SNAKES)):
+    for i in range (0, len(config.SNAKES)):
         if pos == config.SNAKES[i][0]:
             print(title + " gets snake from " + str(config.SNAKES[i][0]) + " to "  + 
             str(config.SNAKES[i][1]))
---
+++ testing.py
@@ -0,0 +1,3 @@
+import config
+import NoughtsAndCrosses.py
+print(FindPos(25))
